49fbf18989e0fffd85f972a65ae152e94ede5422
SophieOps <sophie.opsommer@gmail.com>
2016-03-30 14:25:43 +0200

    with a bit php
    dbut de l'ajout de php au projet

---
+++ menu.php
@@ -0,0 +1,6 @@
+<div class="collection">
+    <a href="home.html" class="collection-item">Accueil</a>
+    <a href="simulation.html" class="collection-item">Simulation</a>
+    <a href="stat.html" class="collection-item">Statistiques</a>
+    <a href="config.html" class="collection-item">Paramètre</a>
+</div>
